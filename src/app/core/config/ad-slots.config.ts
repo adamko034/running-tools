cb894c95dfa38133f1c539fea7137bf9d18dab34
adamko <adamko034@outlook.com>
2025-07-30 13:17:29 +0200

    removed sidebar ad

--- src/app/core/config/ad-slots.config.ts
+++ src/app/core/config/ad-slots.config.ts
@@ -12,5 +12,6 @@ export const AD_SLOTS = {
   // Tool Pages (via tool-view template)
   TOOL_AFTER_CONTENT: '1828346088',
   TOOL_CONTENT_BREAK: '9115593567',
+  TOOL_CONTENT_BREAK_2: '6533678168',
   TOOL_SIDEBAR: '3332999442',
 } as const;
