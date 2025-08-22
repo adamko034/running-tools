e171b926d4d44724c10b22402faf150f31349799
adamko <adamko034@outlook.com>
2025-08-22 08:30:01 +0200

    Bump version to 1.0.54

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  version: '1.0.53'
+  version: '1.0.54'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  version: '1.0.53'
+  version: '1.0.54'
 };
